09aed075fc79e66edfb16722da243847eb2c4dce
Nick Sarkauskas <nsarka00@gmail.com>
2026-03-08 14:54:19 -0400

    Fix orientation of tilesheet

--- assets/tiled/Sample.tsx
+++ assets/tiled/Sample.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Sample" tilewidth="256" tileheight="512" tilecount="11" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
+ <grid orientation="isometric" width="1" height="1"/>
  <tile id="12">
   <image source="Sprites/crate_E.png" width="256" height="512"/>
  </tile>
